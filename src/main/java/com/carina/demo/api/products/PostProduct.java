e56da2684b84f1014923271ee2e5caf39c0e45e4
Mazzaroli <carlitosmazaroli@gmail.com>
2024-02-04 20:50:12 -0300

    feat: Add PostProduct API method
    Description:
    This commit adds the `PostProduct` class, extending `AbstractApiMethodV2`, representing an API method for posting a new product using the HTTP POST method. The class is annotated with endpoint details, specifying the HTTP POST method and successful HTTP status. It includes methods for initializing the API request.
    
    Changes:
    - Added `PostProduct` class.
    - Extended `AbstractApiMethodV2` for API method implementation.
    - Annotated the class with endpoint details, specifying the HTTP POST method and successful HTTP status.
    - Implemented methods for initializing the API request.
    
    Context:
    The `PostProduct` class provides the capability to post a new product via API, enhancing the flexibility and maintainability of test scenarios.

---
+++ src/main/java/com/carina/demo/api/products/PostProduct.java
@@ -0,0 +1,21 @@
+package com.carina.demo.api.products;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.RequestTemplatePath;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.config.Configuration;
+
+@Endpoint(url = "${base_url}/products/add", methodType = HttpMethodType.POST)
+@RequestTemplatePath(path = "api/products/_post/rq.schema")
+@ResponseTemplatePath(path = "api/products/_post/rs.schema")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class PostProduct extends AbstractApiMethodV2 {
+
+    public PostProduct() {
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+    }
+}
